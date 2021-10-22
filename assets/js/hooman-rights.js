31436991f903ea25052012633f4bbbdc7e41e13e
BG <bhaskaragautama@gmail.com>
2021-10-22 17:15:46 +0800

    finish at some point

--- assets/js/hooman-rights.js
+++ assets/js/hooman-rights.js
@@ -1,4 +1,12 @@
+// const PATH = '/git/kuliah';
+// const HOME = 'http://localhost/git/kuliah/';
+const PATH = '/kuliah';
+const HOME = 'https://bhaskaragautama.github.io/kuliah/';
 var hoom = [
    'eb9ef16e235842e33446b6ffc92ce309',
-   'ab404fe0f7beb50d60822e60443ac746'
+   'ab404fe0f7beb50d60822e60443ac746',
+   'd4004789de896405aac2214059391a92',
+   '6bd4f5e7815fda074fdce567c50b4f84',
+   'd6293fdb64b6d53a4e98e75d9216ebbf',
+   '011c65a27dbf4e6165c4f35a3d50d2fc'
 ];
