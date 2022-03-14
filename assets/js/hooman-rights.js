3ed0c17c01b4819f652586d0521d1ccfc9c8e3b5
bhaskaragautama <bhaskaragautama@gmail.com>
2022-03-14 10:33:55 +0800

    new semester

--- assets/js/hooman-rights.js
+++ assets/js/hooman-rights.js
@@ -4,9 +4,6 @@ const PATH = '/kuliah';
 const HOME = 'https://bhaskaragautama.github.io/kuliah/';
 var hoom = [
    'eb9ef16e235842e33446b6ffc92ce309',
-   'ab404fe0f7beb50d60822e60443ac746',
-   'd4004789de896405aac2214059391a92',
-   '6bd4f5e7815fda074fdce567c50b4f84',
-   'd6293fdb64b6d53a4e98e75d9216ebbf',
-   '011c65a27dbf4e6165c4f35a3d50d2fc'
+   '41ab373d764ccecc79a7af47bf491792',
+   '00e7b0f4f4465fc748de709c27a9b741'
 ];
